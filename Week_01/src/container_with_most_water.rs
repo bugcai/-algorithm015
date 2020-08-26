e96b44b38fee1356bee01e4f4c7cdfd9fb878e2a
kilik <samuel_tsai@hotmail.com>
2020-08-26 15:17:36 +0800

    enhance readability for climb_stairs

---
+++ Week_01/src/container_with_most_water.rs
@@ -0,0 +1,29 @@
+struct Solution;
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let (mut i, mut j, mut max_area) = (0, height.len() - 1, 0);
+        while i < j {
+            let hi = height[i];
+            let hj = height[j];
+            max_area = max_area.max(hi.min(hj) * (j - i) as i32);
+            if hi < hj {
+                i += 1;
+            } else {
+                j -= 1;
+            }
+        }
+        max_area
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn regular() {
+        let height = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
+        assert_eq!(Solution::max_area(height), 49);
+    }
+}
--- Week_01/src/main.rs
+++ Week_01/src/main.rs
@@ -1,6 +1,7 @@
 mod climbing_stairs;
 mod move_zeros;
 mod plus_one;
+mod container_with_most_water;
 
 
 fn main() {}
