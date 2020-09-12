c8def2f43c8c95fa66064cbfe3b43e22dafd869e
kilik <samuel_tsai@hotmail.com>
2020-09-12 15:44:27 +0800

    双指针法求解两数和问题

--- Week_02/src/two_sum.rs
+++ Week_02/src/two_sum.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 struct Solution;
 
+// 一遍哈希表
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map: HashMap<i32, usize> = HashMap::new();
@@ -15,6 +16,34 @@ impl Solution {
     }
 }
 
+struct Solution2;
+
+// 双指针法
+impl Solution2 {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut tmp = nums.clone();
+        tmp.sort();
+        let mut res = vec![];
+        let (mut lo, mut hi) = (0, tmp.len() - 1);
+        while lo < hi {
+            let sum = tmp[lo] + tmp[hi];
+            if sum > target {
+                hi -= 1;
+            } else if sum < target {
+                lo += 1;
+            } else {
+                for k in 0..nums.len() {
+                    if nums[k] == tmp[lo] || nums[k] == tmp[hi] {
+                        res.push(k as i32);
+                    }
+                }
+                return res;
+            }
+        }
+        panic!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +51,6 @@ mod tests {
     #[test]
     fn case() {
         assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(Solution2::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
     }
 }
