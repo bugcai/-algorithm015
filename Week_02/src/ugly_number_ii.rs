fd8c4c69870258057479018b97515850c4f25038
kilik <samuel_tsai@hotmail.com>
2020-09-22 18:31:46 +0800

    no comments

--- Week_02/src/ugly_number_ii.rs
+++ Week_02/src/ugly_number_ii.rs
@@ -2,22 +2,16 @@ struct Solution;
 
 impl Solution {
     pub fn nth_ugly_number(n: i32) -> i32 {
-        let mut ugly = vec![1];
-        let (mut p2, mut p3, mut p5) = (0, 0, 0);
+        let mut ugly_nums = vec![1];
         let mut min = 1;
+        let (mut p2, mut p3, mut p5) = (0, 0, 0);
         for _ in 2..=n {
-            let (u2, u3, u5) = (ugly[p2] * 2, ugly[p3] * 3, ugly[p5] * 5);
+            let (u2, u3, u5) = (2 * ugly_nums[p2], 3 * ugly_nums[p3], 5 * ugly_nums[p5]);
             min = u2.min(u3).min(u5);
-            ugly.push(min);
-            if u2 == min {
-                p2 += 1;
-            }
-            if u3 == min {
-                p3 += 1;
-            }
-            if u5 == min {
-                p5 += 1;
-            }
+            ugly_nums.push(min);
+            if u2 == min { p2 += 1; }
+            if u3 == min { p3 += 1; }
+            if u5 == min { p5 += 1; }
         }
         min
     }
